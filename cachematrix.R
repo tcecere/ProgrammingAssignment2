1b8f9045a5136c21d86d8d95e0d1ff45d5673d52
tcecere <tcecere@blackmountconsulting.com>
2015-03-19 12:17:29 -0400

    Initial creation of my version of chachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+##T.Cecere
+##Mar.19.15
+## Take an invertible matrix and create a list of functions
+## use "Global" assignment in parent environment (<<-) for matrix and eventual inverse
 
-## Write a short comment describing this function
+ 
+makeCacheMatrix <- function(x = matrix()) { 
+     ##First time through, set everything to NULL
+     inv <- NULL
+     set <- function(y) {
+          x <<- y
+          inv <<- NULL
+     }
+     ##Create and return list of functions to be used by cacheSolve()
+     get <- function() x
+     setinverse <- function(solve) inv <<- solve
+     getinverse <- function() inv
+     list(set = set, get = get,
+          setinverse = setinverse,
+          getinverse = getinverse)
+     
+ } 
+ 
 
-makeCacheMatrix <- function(x = matrix()) {
+## Use list of functions from makeCacheMatrix
+## Return a matrix that is the inverse of 'x' 
 
-}
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+cacheSolve <- function(x, ...) { 
+     ##Check to see if Inverse is already in memory, warn if not calculating
+     inv <- x$getinverse()
+     if(!is.null(inv)) {
+          message("getting cached data")
+          return(inv)
+     }
+     ##inv is null, take data from get(), calculate inverse, &
+     ##put it into memory using setinverse(). Return answer.
+     data <- x$get()
+     inv <- solve(data, ...)
+     x$setinverse(inv)
+     inv
+} 
